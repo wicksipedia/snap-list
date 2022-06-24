c443e7c39e0df24c2fa4560c4abd1e5be1d6489b
Matt Wicks <matt.wicks@gmail.com>
2022-06-24 11:43:55 +1000

    added snap code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+  const [canSnap, setCanSnap] = useState(false);
+
+  const handleMessageChange = event => {
+    setMessage(event.target.value);
+    setCanSnap(message.split('\n').length > 1);
+  };
+
+  const snapClicked = () => {
+    const items = message.split('\n');
+    const snapped = theSnap(items);
+    setMessage(snapped.join('\n'));
+  };
+
+  const theSnap = items => {
+    const numberToSnap = Math.floor(items.length / 2);
+    for (let i = 0; i < numberToSnap; i++) {
+      items.splice(generateRandomInteger(0, items.length), 1);
+    }
+    return items;
+  };
+
+  const generateRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter the list and *snap* 🫰 away
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleMessageChange}
+          />
+        </div>
+
+        {canSnap && (
+          <div>
+            <button onClick={snapClicked}>
+              🫰
+            </button>
+          </div>
+        )}
+
+        <div>
+          <p>{message}</p>
+        </div>
       </header>
     </div>
   );
